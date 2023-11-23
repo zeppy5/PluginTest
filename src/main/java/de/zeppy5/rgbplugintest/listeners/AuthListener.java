f920b9ac0d36e1b17c3e12d9c715b05b9c5c8a37
BuildTools <unconfigured@null.spigotmc.org>
2023-11-23 19:03:07 +0100

    first version

--- src/main/java/de/zeppy5/rgbplugintest/listeners/AuthListener.java
+++ src/main/java/de/zeppy5/rgbplugintest/listeners/AuthListener.java
@@ -2,7 +2,6 @@ package de.zeppy5.rgbplugintest.listeners;
 
 import de.zeppy5.rgbplugintest.RGBPluginTest;
 import de.zeppy5.rgbplugintest.util.ServerPlayer;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
